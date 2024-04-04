5f6a4972bab285ab7fcb0921df07c82c144a3844
Kasim Kaizer <155087257+KasimKaizer@users.noreply.github.com>
2024-04-04 17:55:57 +0400

    write tests for tik tac toe

--- tic_tac_toe/game.go
+++ tic_tac_toe/game.go
@@ -1,1 +1,12 @@
 package tictactoe
+
+type player int
+
+const (
+	X = iota + 1
+	Y
+)
+
+func PlayGames(games [][]int) int {
+	panic("implement this")
+}
--- tic_tac_toe/game_test.go
+++ tic_tac_toe/game_test.go
@@ -1,5 +1,13 @@
 package tictactoe
 
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+)
+
 var TestCases = []struct {
 	Description string
 	InputFile   string
@@ -10,4 +18,45 @@ var TestCases = []struct {
 		"base_test.txt",
 		1,
 	},
+}
+
+func parseData(filePath string) ([][]int, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	var games [][]int
+	for scanner.Scan() {
+		splitText := strings.Fields(scanner.Text())
+		var game []int
+		for _, numChar := range splitText {
+			num, err := strconv.Atoi(numChar)
+			if err != nil {
+				return nil, err
+			}
+			game = append(game, int(num))
+		}
+		games = append(games, game)
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return games, nil
+}
+
+func TestPlayGames(t *testing.T) {
+	for _, tt := range TestCases {
+		t.Run(tt.Description, func(t *testing.T) {
+			data, err := parseData(tt.InputFile)
+			if err != nil {
+				t.Fatal(err)
+			}
+			got := PlayGames(data)
+			if got != tt.Expected {
+				t.Fatalf("expected: %d, got: %d", tt.Expected, got)
+			}
+		})
+	}
 }
