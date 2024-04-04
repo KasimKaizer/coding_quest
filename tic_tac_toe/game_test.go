012b7b17372d71da49ebda28d44c8ddb7bd752c0
Kasim Kaizer <155087257+KasimKaizer@users.noreply.github.com>
2024-04-04 17:07:42 +0400

    tic tac toe stud

---
+++ tic_tac_toe/game_test.go
@@ -0,0 +1,13 @@
+package tictactoe
+
+var TestCases = []struct {
+	Description string
+	InputFile   string
+	Expected    int
+}{
+	{
+		"Base Test Case",
+		"base_test.txt",
+		1,
+	},
+}
---
+++ tic_tac_toe/go.mod
@@ -0,0 +1,3 @@
+module coding_quest/tic_tac_toe
+
+go 1.22.1
