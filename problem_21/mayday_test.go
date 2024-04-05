67c3a8bb0c080afb0188944c3394b5d1a15201d9
Kasim Kaizer <155087257+KasimKaizer@users.noreply.github.com>
2024-04-05 16:44:46 +0400

    Update: solve problem 21

--- problem_21/mayday_test.go
+++ problem_21/mayday_test.go
@@ -14,6 +14,11 @@ var TestCases = []struct {
 		"base_test.txt",
 		"This is a test. This is a test. Thankyou.",
 	},
+	{
+		"Real test case",
+		"real_test.txt",
+		"This is the SY Titanic II. We have struck an asteroid. Engines non responsive. Please help. Coordinates 242.03 by 182.24 by 92.58. Message ends.",
+	},
 }
 
 func TestDecodeMessage(t *testing.T) {
