d3672127a5e615d1fdfc014a0e3ceada9b74d266
Kasim Kaizer <155087257+KasimKaizer@users.noreply.github.com>
2024-04-08 01:25:12 +0400

    Add: add tests and a template for problem 23

---
+++ problem_23/asteroids.go
@@ -0,0 +1,13 @@
+package asteroids
+
+type Asteroid struct {
+	XPos, YPos, XSpeed, YSpeed float64
+}
+
+// don't overthink questions, there might be a better algorithmic solution to this problem
+// but I am not smart enough to figure it out, so after a lot of time searching, I have chosen
+// just use the brute force approach
+
+func FindSafePath([]Asteroid) string {
+	panic("implement this")
+}
---
+++ problem_23/asteroids_test.go
@@ -0,0 +1,68 @@
+package asteroids
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+var TestCases = []struct {
+	Description string
+	InputFile   string
+	Expected    string
+}{
+	{
+		"Base Test Cases",
+		"base_test.txt",
+		"5:4",
+	},
+}
+
+func parseData(filePath string) ([]Asteroid, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	output := make([]Asteroid, 0)
+	for scanner.Scan() {
+		splitNum := strings.Fields(scanner.Text())
+		var temp []float64
+		for _, charNum := range splitNum {
+			num, err := strconv.ParseFloat(charNum, 64)
+			if err != nil {
+				return nil, err
+			}
+			temp = append(temp, num)
+		}
+		asteroid := Asteroid{
+			XPos:   temp[0],
+			YPos:   temp[1],
+			XSpeed: temp[2],
+			YSpeed: temp[3],
+		}
+		output = append(output, asteroid)
+	}
+	if err = scanner.Err(); err != nil {
+		return nil, err
+	}
+	return output, nil
+}
+
+func TestFindSafePath(t *testing.T) {
+	for _, tt := range TestCases {
+		t.Run(tt.Description, func(t *testing.T) {
+			data, err := parseData(tt.InputFile)
+			if err != nil {
+				t.Error(err)
+			}
+			got := FindSafePath(data)
+			if tt.Expected != got {
+				t.Fatalf("expected: %s,got: %s", tt.Expected, got)
+			}
+		})
+	}
+}
